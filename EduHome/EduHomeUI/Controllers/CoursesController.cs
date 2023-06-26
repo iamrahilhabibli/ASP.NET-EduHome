fceddc07f8827c080830f167bf9f85df6178c4de
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 21:09:33 +0400

    Course Index View Done

--- EduHome/EduHomeUI/Controllers/CoursesController.cs
+++ EduHome/EduHomeUI/Controllers/CoursesController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.DataAccess.Contexts;
+using EduHomeUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Controllers;
 
 public class CoursesController : Controller
 {
-	public IActionResult Index()
+	private readonly AppDbContext _context;
+
+    public CoursesController(AppDbContext context)
+    {
+		_context = context;	
+    }
+    public async Task<IActionResult> Index()
 	{
-		return View();
+		CourseVM courseVM = new()
+		{
+			Courses = await _context.courses.ToArrayAsync(),
+		};
+		return View(courseVM);
 	}
 }
---
+++ EduHome/EduHomeUI/ViewModels/CourseVM.cs
@@ -0,0 +1,8 @@
+﻿using EduHome.Core.Entities;
+
+namespace EduHomeUI.ViewModels;
+
+public class CourseVM
+{
+	public IEnumerable<Courses> Courses { get; set; }
+}
