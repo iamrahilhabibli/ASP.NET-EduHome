3afb5ee1ea788f013889b930517787ec7dcea714
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 13:13:07 +0400

    Notices isLeft Column Added

--- EduHome/EduHome.Core/Entities/Notices.cs
+++ EduHome/EduHome.Core/Entities/Notices.cs
@@ -7,4 +7,5 @@ public class Notices:IEntity
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public string Description { get; set; }
+    public bool isLeft { get; set; }
 }
---
+++ EduHome/EduHome.DataAccess/Migrations/20230626091219_IsLeftColAdded.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduHome.DataAccess.Migrations
+{
+    public partial class IsLeftColAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "notices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isLeft = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_notices", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "notices");
+        }
+    }
+}
