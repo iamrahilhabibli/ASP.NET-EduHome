c060df860dd5bcf5513cb771f173b208c300ace9
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 21:26:16 +0400

    Teacher TeacherDetails Classes Added

---
+++ EduHome/EduHome.Core/Entities/TeacherDetails.cs
@@ -0,0 +1,14 @@
+﻿using EduHome.Core.Interfaces;
+
+namespace EduHome.Core.Entities
+{
+	public class TeacherDetails:IEntity
+	{
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Degree { get; set; }
+        public int ExperienceYears { get; set; }
+        public string Hobbies { get; set; }
+        public string Faculty { get; set; }
+    }
+}
---
+++ EduHome/EduHome.Core/Entities/Teachers.cs
@@ -0,0 +1,11 @@
+﻿using EduHome.Core.Interfaces;
+
+namespace EduHome.Core.Entities;
+
+public class Teachers:IEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public string ImagePath { get; set; }
+}
