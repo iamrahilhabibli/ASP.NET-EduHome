8c996c0c0e735ffc8fb64d707a6c053c9e65b369
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 21:41:41 +0400

    About and Teachers View Added

---
+++ EduHome/EduHome.DataAccess/Migrations/20230626173700_TeachersAndTeacherDetailsAdded.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduHome.DataAccess.Migrations
+{
+    public partial class TeachersAndTeacherDetailsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teachers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "teachersDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Degree = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExperienceYears = table.Column<int>(type: "int", nullable: false),
+                    Hobbies = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Faculty = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teachersDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_teachersDetails_teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "teachers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_teachersDetails_TeacherId",
+                table: "teachersDetails",
+                column: "TeacherId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "teachersDetails");
+
+            migrationBuilder.DropTable(
+                name: "teachers");
+        }
+    }
+}
--- EduHome/EduHomeUI/Controllers/AboutController.cs
+++ EduHome/EduHomeUI/Controllers/AboutController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EduHomeUI.Controllers;
 
 public class AboutController : Controller
 {
+	private readonly AppDbContext _context;
 
-	public IActionResult Index()
+    public AboutController(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
 	{
 		return View();
 	}
