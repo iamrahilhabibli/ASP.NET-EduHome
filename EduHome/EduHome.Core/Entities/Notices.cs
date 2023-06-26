f88ff8cf246ff685d8648f31efc2ffdfd7642127
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 14:27:13 +0400

    Notice-right start

--- EduHome/EduHome.Core/Entities/Notices.cs
+++ EduHome/EduHome.Core/Entities/Notices.cs
@@ -5,7 +5,7 @@ namespace EduHome.Core.Entities;
 public class Notices:IEntity
 {
     public int Id { get; set; }
-    public DateTime? Date { get; set; }
+    public DateTime? Date { get; set; } = DateTime.MinValue;
     public string Description { get; set; }
     public bool isLeft { get; set; }
     public string? Title { get; set; }
