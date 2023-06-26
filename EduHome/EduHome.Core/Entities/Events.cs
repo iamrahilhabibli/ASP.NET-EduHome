b807e3667244ef62d4b8dadf69f5db19a2f29d94
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 18:14:13 +0400

    Events Class added

---
+++ EduHome/EduHome.Core/Entities/Events.cs
@@ -0,0 +1,13 @@
+﻿using EduHome.Core.Interfaces;
+
+namespace EduHome.Core.Entities;
+
+public class Events:IEntity
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Location { get; set; }
+}
