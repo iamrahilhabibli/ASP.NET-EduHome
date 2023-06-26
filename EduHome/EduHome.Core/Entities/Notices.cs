36e2b7c54a1b573051019c19fdf8de96decf9314
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 13:33:49 +0400

    Database data added

--- EduHome/EduHome.Core/Entities/Notices.cs
+++ EduHome/EduHome.Core/Entities/Notices.cs
@@ -5,8 +5,8 @@ namespace EduHome.Core.Entities;
 public class Notices:IEntity
 {
     public int Id { get; set; }
-    public DateTime Date { get; set; }
+    public DateTime? Date { get; set; }
     public string Description { get; set; }
     public bool isLeft { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
 }
