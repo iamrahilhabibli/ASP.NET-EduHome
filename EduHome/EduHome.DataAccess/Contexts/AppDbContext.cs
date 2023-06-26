5bdac2fecb6bd31ff9c63d2049fcc9afab2f46a9
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 10:43:40 +0400

    Db Context Config Done

--- EduHome/EduHome.DataAccess/Contexts/AppDbContext.cs
+++ EduHome/EduHome.DataAccess/Contexts/AppDbContext.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace EduHome.DataAccess.Contexts
 {
-	internal class AppDbContext
+	public class AppDbContext:DbContext
 	{
-	}
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+    }
 }
--- EduHome/EduHomeUI/Program.cs
+++ EduHome/EduHomeUI/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using EduHome.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+);
+app.MapControllerRoute(
+    name: "Default",
+    pattern: "{controller=Blog}/{action=Index}/{Id?}"
+);
 
 app.Run();
