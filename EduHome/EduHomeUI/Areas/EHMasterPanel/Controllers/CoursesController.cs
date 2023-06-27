c00af75b94436f6a05dd52dd173c50894c9e3509
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 00:20:26 +0400

    Admin Panel Courses View Start

---
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/CoursesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduHomeUI.Areas.EHMasterPanel.Controllers;
+
+public class CoursesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
