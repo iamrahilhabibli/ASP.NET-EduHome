fd740b3bd8317b167be78b6f8246111cc06b7b68
Rahil Habibli <rahilah@code.edu.az>
2023-06-27 23:41:51 +0400

    Sign up done

--- EduHome/EduHome.Core/Entities/Users.cs
+++ EduHome/EduHome.Core/Entities/Users.cs
@@ -8,6 +8,10 @@ public class Users:IEntity
     public int Id { get; set; }
     [Required,MaxLength(30)]
     public string Username { get; set; }
-    [Required,MaxLength(30)]
+    [Required]
+    [DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).*$",
+       ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol.")]
+    [MaxLength(30)]
     public string Password { get; set; }
 }
