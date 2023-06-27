ae893cc1aa6a076cf258258d638bca94111fc19e
Rahil Habibli <rahilah@code.edu.az>
2023-06-27 16:12:47 +0400

    User Replies Class Added

---
+++ EduHome/EduHome.Core/Entities/UserReplies.cs
@@ -0,0 +1,17 @@
+﻿using EduHome.Core.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduHome.Core.Entities;
+
+public class UserReplies:IEntity
+{
+    public int Id { get; set; }
+    [Required,MaxLength(50)]
+    public string Name { get; set; }
+    [Required,MaxLength(75)]
+    public string Email { get; set; }
+    [Required,MaxLength(50)]
+    public string Subject { get; set; }
+    [Required,MaxLength(250)]
+    public string Message { get; set; }
+}
