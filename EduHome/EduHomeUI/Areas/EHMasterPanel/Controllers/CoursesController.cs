41571f21d68bb93d27cdd7e83af0a8a5647ad4a9
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 01:01:56 +0400

    Course Read Added Admin Panel

--- EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/CoursesController.cs
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/CoursesController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Areas.EHMasterPanel.Controllers;
-
+[Area("EHMasterPanel")]
 public class CoursesController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
+	private readonly AppDbContext _context;
+	public CoursesController(AppDbContext context)
+	{
+		_context = context;
+	}
+	public async Task<IActionResult> Index()
+	{
+		return View(await _context.courses.ToListAsync());
+	}
+	public IActionResult Create()
+	{
+		return View();
+	}
 }
