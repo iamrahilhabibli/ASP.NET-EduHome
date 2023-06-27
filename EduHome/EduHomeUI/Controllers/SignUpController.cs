7b6e6a418e231151a5e429761f09a4150aeeb422
Rahil Habibli <rahilah@code.edu.az>
2023-06-27 23:19:44 +0400

    SignUp Done

--- EduHome/EduHomeUI/Controllers/SignUpController.cs
+++ EduHome/EduHomeUI/Controllers/SignUpController.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.Core.Entities;
+using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EduHomeUI.Controllers
 {
     public class SignUpController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public SignUpController(AppDbContext context)
+        {
+            _context = context; 
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Users user)
+        {
+       
+            if (ModelState.IsValid)
+            {
+                _context.users.Add(user);
+                _context.SaveChanges();
+                TempData["Success"] = "Category Created Successfully";
+                return RedirectToAction(nameof(Index));
+            }
             return View();
         }
     }
