023c9a3c2f9eb04c268c12bf62e9583dd48deabe
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 00:14:42 +0400

    Login to admin panel added

--- EduHome/EduHomeUI/Controllers/LoginController.cs
+++ EduHome/EduHomeUI/Controllers/LoginController.cs
@@ -1,11 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.Core.Entities;
+using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Controllers;
 
 public class LoginController : Controller
 {
+    private readonly AppDbContext _context;
+
+    public LoginController(AppDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
         return View();
+    }
+    [HttpPost]
+    public IActionResult Login(Users user)
+    {
+        if (ModelState.IsValid)
+        {
+            bool isAuthenticated = AuthenticateUser(user.Username, user.Password);
+
+            if (isAuthenticated)
+            {
+                bool isAdmin = CheckAdminStatus(user.Username);
+                if (isAdmin)
+                {
+                    return RedirectToAction("Index", "Dashboard", new { area = "EHMasterPanel" });
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid username or password");
+        }
+
+        return View(user);
+    }
+    private bool CheckAdminStatus(string username)
+    {
+        bool isAdmin = _context.users.Any(u => u.Username == username && u.IsAdmin);
+        return isAdmin;
+    }
+    private bool AuthenticateUser(string username, string password)
+    {
+        bool isAuthenticated = _context.users.Any(u => u.Username == username && u.Password == password);
+        return isAuthenticated;
     }
 }
