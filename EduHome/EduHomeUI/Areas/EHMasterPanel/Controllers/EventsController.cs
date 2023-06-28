1f5ce0ca3d7877f8f01bc1b2b69a59f9aa8956fc
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 16:47:52 +0400

    Events Table View Done

--- EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/EventsController.cs
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/EventsController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Areas.EHMasterPanel.Controllers;
-
+[Area("EHMasterPanel")]
 public class EventsController : Controller
 {
-	public IActionResult Index()
+	private readonly AppDbContext _context;
+
+    public EventsController(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> Index()
 	{
-		return View();
+		return View(await _context.events.ToListAsync());
 	}
 }
