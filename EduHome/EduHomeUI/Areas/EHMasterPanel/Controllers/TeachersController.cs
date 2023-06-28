8706b4e92165d0f9b5bc6d391c59e1319ed3a294
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 23:08:31 +0400

    Admin panel teachers view start

---
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/TeachersController.cs
@@ -0,0 +1,21 @@
+﻿using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHomeUI.Areas.EHMasterPanel.Controllers
+{
+    [Area("EHMasterPanel")]
+    public class TeachersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TeachersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+			return View(await _context.teachers.ToListAsync());
+		}
+    }
+}
