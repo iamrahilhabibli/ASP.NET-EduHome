6474663146b6177ceb8b632711344560ca02ccec
Rahil Habibli <rahilah@code.edu.az>
2023-06-29 00:08:57 +0400

    TeacherVIewModel Range added

--- EduHome/EduHomeUI/ViewModels/TeachersViewModel.cs
+++ EduHome/EduHomeUI/ViewModels/TeachersViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace EduHomeUI.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHomeUI.ViewModels
 {
 	public class TeachersViewModel
 	{
@@ -13,11 +15,23 @@
 		public string Email { get; set; }
 		public string PhoneNumber { get; set; }
 		public string SkypeAddress { get; set; }
+
+		[Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
 		public int LanguageSkills { get; set; }
+
+		[Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
 		public int TeamLeaderSkills { get; set; }
+
+		[Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
 		public int DevelopmentSkills { get; set; }
+
+		[Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
 		public int Design { get; set; }
+
+		[Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
 		public int Innovation { get; set; }
+
+		[Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
 		public int Communication { get; set; }
 	}
 }
