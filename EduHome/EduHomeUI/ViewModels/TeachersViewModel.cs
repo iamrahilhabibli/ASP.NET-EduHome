4a950ff3401714858283adf1d230a779eb0d64c0
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 23:15:30 +0400

    TeachersViewModel Done

---
+++ EduHome/EduHomeUI/ViewModels/TeachersViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace EduHomeUI.ViewModels
+{
+	public class TeachersViewModel
+	{
+		public string Name { get; set; }
+		public string Position { get; set; }
+		public string ImagePath { get; set; }
+		public string Description { get; set; }
+		public string Degree { get; set; }
+		public int ExperienceYears { get; set; }
+		public string Hobbies { get; set; }
+		public string Faculty { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+		public string SkypeAddress { get; set; }
+		public int LanguageSkills { get; set; }
+		public int TeamLeaderSkills { get; set; }
+		public int DevelopmentSkills { get; set; }
+		public int Design { get; set; }
+		public int Innovation { get; set; }
+		public int Communication { get; set; }
+	}
+}
