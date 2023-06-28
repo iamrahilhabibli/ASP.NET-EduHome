ea01b306c669084927f45ef4169950a01f76f6fe
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 17:06:36 +0400

    Create Events view done

---
+++ EduHome/EduHomeUI/ViewModels/EventsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EduHomeUI.ViewModels;
+
+public class EventsViewModel
+{
+	public string Title { get; set; }
+	public DateTime Date { get; set; }
+	public DateTime StartTime { get; set; }
+	public DateTime EndTime { get; set; }
+	public string Location { get; set; }
+}
