163146f06ab8a5ad038492070f78bfc522306ed7
Rahil Habibli <rahilah@code.edu.az>
2023-06-28 17:13:56 +0400

    Events create page

--- EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/EventsController.cs
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/EventsController.cs
@@ -1,4 +1,7 @@
-﻿using EduHome.DataAccess.Contexts;
+﻿using AutoMapper;
+using EduHome.Core.Entities;
+using EduHome.DataAccess.Contexts;
+using EduHomeUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,13 +10,35 @@ namespace EduHomeUI.Areas.EHMasterPanel.Controllers;
 public class EventsController : Controller
 {
 	private readonly AppDbContext _context;
+	private readonly IMapper _mapper;
 
-    public EventsController(AppDbContext context)
+    public EventsController(AppDbContext context,IMapper mapper)
     {
         _context = context;
+		_mapper = mapper;
     }
     public async Task<IActionResult> Index()
 	{
 		return View(await _context.events.ToListAsync());
+	}
+	public IActionResult Create()
+	{
+		return View();
+	}
+	[HttpPost]
+	[AutoValidateAntiforgeryToken]
+	public async Task<IActionResult> Create(EventsViewModel events)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View();
+		}
+
+		Events newEvent = _mapper.Map<Events>(events);
+		await _context.events.AddAsync(newEvent);
+		await _context.SaveChangesAsync();
+		TempData["Success"] = "Event Created Successfully";
+
+		return RedirectToAction(nameof(Index));
 	}
 }
