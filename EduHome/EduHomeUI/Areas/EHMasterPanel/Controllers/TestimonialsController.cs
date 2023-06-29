b1b4dca760b3e7d616eba89d34b0d676483cc6b4
Rahil Habibli <rahilah@code.edu.az>
2023-06-29 22:56:50 +0400

    Testimonials View done

---
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/TestimonialsController.cs
@@ -0,0 +1,19 @@
+﻿using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHomeUI.Areas.EHMasterPanel.Controllers;
+[Area("EHMasterPanel")]
+public class TestimonialsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public TestimonialsController(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> IndexAsync()
+    {
+        return View(await _context.testimonials.ToListAsync());
+    }
+}
