17baed838896582524e8160e4ef866dc2352f3f9
Rahil Habibli <rahilah@code.edu.az>
2023-06-30 12:52:39 +0400

    Update Events DOne

--- EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/EventsController.cs
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/EventsController.cs
@@ -12,12 +12,12 @@ public class EventsController : Controller
 	private readonly AppDbContext _context;
 	private readonly IMapper _mapper;
 
-    public EventsController(AppDbContext context,IMapper mapper)
-    {
-        _context = context;
+	public EventsController(AppDbContext context, IMapper mapper)
+	{
+		_context = context;
 		_mapper = mapper;
-    }
-    public async Task<IActionResult> Index()
+	}
+	public async Task<IActionResult> Index()
 	{
 		return View(await _context.events.ToListAsync());
 	}
@@ -26,16 +26,16 @@ public class EventsController : Controller
 		return View();
 	}
 
-    [HttpPost]
-    [AutoValidateAntiforgeryToken]
-    public async Task<IActionResult> Create(EventsViewModel events)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View(events);
-        }
+	[HttpPost]
+	[AutoValidateAntiforgeryToken]
+	public async Task<IActionResult> Create(EventsViewModel events)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(events);
+		}
 
-        Events newEvent = _mapper.Map<Events>(events);
+		Events newEvent = _mapper.Map<Events>(events);
 
 		EventDetails details = new EventDetails
 		{
@@ -45,16 +45,16 @@ public class EventsController : Controller
 
 		newEvent.EventDetails = details;
 
-        await _context.events.AddAsync(newEvent);
-        await _context.SaveChangesAsync();
+		await _context.events.AddAsync(newEvent);
+		await _context.SaveChangesAsync();
 
-        TempData["Success"] = "Event Created Successfully";
+		TempData["Success"] = "Event Created Successfully";
 
-        return RedirectToAction(nameof(Index));
-    }
+		return RedirectToAction(nameof(Index));
+	}
 
 
-    public async Task<IActionResult> Delete(int Id)
+	public async Task<IActionResult> Delete(int Id)
 	{
 		Events newEvent = await _context.events.FindAsync(Id);
 		if (newEvent == null)
@@ -80,64 +80,125 @@ public class EventsController : Controller
 		return RedirectToAction(nameof(Index));
 	}
 
-    public async Task<IActionResult> Update(int id)
-    {
-        Events newEvent = await _context.events.FindAsync(id);
-        if (newEvent == null)
-        {
-            return NotFound();
-        }
+	//    public async Task<IActionResult> Update(int id)
+	//    {
+	//        Events newEvent = await _context.events.FindAsync(id);
+	//        if (newEvent == null)
+	//        {
+	//            return NotFound();
+	//        }
 
-        EventsViewModel eventViewModel = _mapper.Map<EventsViewModel>(newEvent);
-        return View(eventViewModel);
-    }
+	//        EventsViewModel eventViewModel = _mapper.Map<EventsViewModel>(newEvent);
+	//        return View(eventViewModel);
+	//    }
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Update(int id, EventsViewModel events)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View(events);
-        }
+	//    [HttpPost]
+	//    [ValidateAntiForgeryToken]
+	//    public async Task<IActionResult> Update(int id, EventsViewModel events)
+	//    {
+	//        if (!ModelState.IsValid)
+	//        {
+	//            return View(events);
+	//        }
 
-        Events newEvent = await _context.events.Include(e => e.EventDetails).FirstOrDefaultAsync(e => e.Id == id);
-        if (newEvent == null)
-        {
-            return NotFound();
-        }
+	//        Events newEvent = await _context.events.Include(e => e.EventDetails).FirstOrDefaultAsync(e => e.Id == id);
+	//        if (newEvent == null)
+	//        {
+	//            return NotFound();
+	//        }
 
-        newEvent.Title = events.Title;
-        newEvent.Date = events.Date;
-        newEvent.StartTime = events.StartTime;
-        newEvent.EndTime = events.EndTime;
-        newEvent.Location = events.Location;
+	//        newEvent.Title = events.Title;
+	//        newEvent.Date = events.Date;
+	//        newEvent.StartTime = events.StartTime;
+	//        newEvent.EndTime = events.EndTime;
+	//        newEvent.Location = events.Location;
 
-        if (newEvent.EventDetails != null)
-        {
-            newEvent.EventDetails.Venue = events.Venue;
-            newEvent.EventDetails.Description = events.Description;
+	//        if (newEvent.EventDetails != null)
+	//        {
+	//            newEvent.EventDetails.Venue = events.Venue;
+	//            newEvent.EventDetails.Description = events.Description;
 
-            _context.Entry(newEvent.EventDetails).State = EntityState.Modified;
-        }
-        else
-        {
-            EventDetails newDetails = new EventDetails
-            {
-                Venue = events.Venue,
-                Description = events.Description
-            };
-            newEvent.EventDetails = newDetails;
-            _context.EventDetails.Add(newDetails);
-        }
+	//            _context.Entry(newEvent.EventDetails).State = EntityState.Modified;
+	//        }
+	//        else
+	//        {
+	//            EventDetails newDetails = new EventDetails
+	//            {
+	//                Venue = events.Venue,
+	//                Description = events.Description
+	//            };
+	//            newEvent.EventDetails = newDetails;
+	//            _context.eventDetails.Add(newDetails);
+	//        }
 
-        _context.Entry(newEvent).State = EntityState.Modified;
-        await _context.SaveChangesAsync();
+	//        _context.Entry(newEvent).State = EntityState.Modified;
+	//        await _context.SaveChangesAsync();
 
-        TempData["Success"] = "Event Updated Successfully";
+	//        TempData["Success"] = "Event Updated Successfully";
 
-        return RedirectToAction(nameof(Index));
-    }
+	//        return RedirectToAction(nameof(Index));
+	//    }
+
+	//
+	public async Task<IActionResult> Update(int id)
+	{
+		Events existingEvent = await _context.events.Include(e => e.EventDetails).FirstOrDefaultAsync(e => e.Id == id);
+		if (existingEvent == null)
+		{
+			return NotFound();
+		}
+
+		EventsViewModel eventViewModel = _mapper.Map<EventsViewModel>(existingEvent);
+		return View(eventViewModel);
+	}
+
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Update(int id, EventsViewModel updatedEvent)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(updatedEvent);
+		}
+
+		Events existingEvent = await _context.events.Include(e => e.EventDetails).FirstOrDefaultAsync(e => e.Id == id);
+		if (existingEvent == null)
+		{
+			return NotFound();
+		}
+
+		existingEvent.Title = updatedEvent.Title;
+		existingEvent.Date = updatedEvent.Date;
+		existingEvent.StartTime = updatedEvent.StartTime;
+		existingEvent.EndTime = updatedEvent.EndTime;
+		existingEvent.Location = updatedEvent.Location;
+
+		if (existingEvent.EventDetails != null)
+		{
+			existingEvent.EventDetails.Venue = updatedEvent.Venue;
+			existingEvent.EventDetails.Description = updatedEvent.Description;
+
+			_context.Entry(existingEvent.EventDetails).State = EntityState.Modified;
+		}
+		else
+		{
+			EventDetails newDetails = new EventDetails
+			{
+				Venue = updatedEvent.Venue,
+				Description = updatedEvent.Description
+			};
+
+			existingEvent.EventDetails = newDetails;
+			_context.eventDetails.Add(newDetails);
+		}
+
+		_context.Entry(existingEvent).State = EntityState.Modified;
+		await _context.SaveChangesAsync();
+
+		TempData["Success"] = "Event Updated Successfully";
+
+		return RedirectToAction(nameof(Index));
+	}
 
 }
 
