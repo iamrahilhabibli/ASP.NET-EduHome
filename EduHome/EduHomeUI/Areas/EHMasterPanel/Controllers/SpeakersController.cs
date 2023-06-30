f52e64606be522391ef6caf457daa558b72263ba
Rahil Habibli <rahilah@code.edu.az>
2023-06-30 23:32:58 +0400

    Speakers create view done

--- EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/SpeakersController.cs
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/SpeakersController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using EduHome.Core.Entities;
 using EduHome.DataAccess.Contexts;
+using EduHomeUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,14 +11,38 @@ namespace EduHomeUI.Areas.EHMasterPanel.Controllers
     public class SpeakersController : Controller
     {
         private readonly AppDbContext _context;
-        private readonly IMapper mapper;
-        public SpeakersController(AppDbContext context)
+        private readonly IMapper _mapper;
+        public SpeakersController(AppDbContext context,IMapper mapper)
         {
             _context = context;
+			_mapper  = mapper;
         }
         public async Task<IActionResult> Index()
         {
             return View(await _context.speakers.ToListAsync());
         }
-    }
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		[AutoValidateAntiforgeryToken]
+		public async Task<IActionResult> Create(SpeakersViewModel speakers)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(speakers);
+			}
+
+			Speakers speaker = _mapper.Map<Speakers>(speakers);
+
+
+			await _context.speakers.AddAsync(speaker);
+			await _context.SaveChangesAsync();
+
+			TempData["Success"] = "Speaker Created Successfully";
+
+			return RedirectToAction(nameof(Index));
+		}
+	}
 }
---
+++ EduHome/EduHomeUI/Mappers/SpeakersProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EduHome.Core.Entities;
+using EduHomeUI.ViewModels;
+
+namespace EduHomeUI.Mappers
+{
+	public class SpeakersProfile:Profile
+	{
+        public SpeakersProfile()
+        {
+            CreateMap<SpeakersViewModel, Speakers>().ReverseMap();
+        }
+    }
+}
