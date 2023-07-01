63287656bebf17b0ab5267a38e4c003f3b9a1457
Rahil Habibli <rahilah@code.edu.az>
2023-07-01 12:29:11 +0400

    Selected Course Drop Down added to testimonial viewmodel

--- EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/TestimonialsController.cs
+++ EduHome/EduHomeUI/Areas/EHMasterPanel/Controllers/TestimonialsController.cs
@@ -1,5 +1,7 @@
 ﻿using EduHome.DataAccess.Contexts;
+using EduHomeUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeUI.Areas.EHMasterPanel.Controllers;
@@ -13,14 +15,27 @@ public class TestimonialsController : Controller
         _context = context;
     }
    public async Task<IActionResult> Index()
-{
-    var testimonials = await _context.testimonials.Include(t => t.Courses).ToListAsync();
-    return View(testimonials);
-}
-
-    public IActionResult Create()
     {
-        return View();
+        var testimonials = await _context.testimonials.Include(t => t.Courses).ToListAsync();
+        return View(testimonials);
     }
-    
+	[HttpGet]
+	public IActionResult Create()
+	{
+		var courses = _context.courses.ToList();
+
+		var availableCourses = courses.Select(c => new SelectListItem
+		{
+			Value = c.Id.ToString(),
+			Text = c.Name
+		}).ToList();
+
+		var viewModel = new TestimonialsViewModel
+		{
+			AvailableCourses = availableCourses
+		};
+
+		return View(viewModel);
+	}
+
 }
--- EduHome/EduHomeUI/ViewModels/TestimonialsViewModel.cs
+++ EduHome/EduHomeUI/ViewModels/TestimonialsViewModel.cs
@@ -1,13 +1,15 @@
-﻿namespace EduHomeUI.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EduHomeUI.ViewModels
 {
 	public class TestimonialsViewModel
 	{
+		public List<SelectListItem> AvailableCourses { get; set; }
+		public string SelectedCourseName { get; set; }
 		public string Name { get; set; }
 		public string Surname { get; set; }
 		public string ImagePath { get; set; }
 		public string Description { get; set; }
 		public string Occupation { get; set; }
-        public string CourseName { get; set; }
-        public string CourseImagePath { get; set; }
-    }
+	}
 }
