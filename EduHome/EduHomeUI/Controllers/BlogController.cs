899d89ac44bffdb662e042f806041bef16afd974
Rahil Habibli <rahilah@code.edu.az>
2023-07-02 17:52:49 +0400

    Blogs - Index View Loop Done - Redirection to Details StarT

--- EduHome/EduHomeUI/Controllers/BlogController.cs
+++ EduHome/EduHomeUI/Controllers/BlogController.cs
@@ -1,18 +1,34 @@
 ﻿using EduHome.DataAccess.Contexts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace EduHomeUI.Controllers
 {
     public class BlogController : Controller
     {
         private readonly AppDbContext _context;
+
         public BlogController(AppDbContext context)
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
-            return View();
+            var blogs = _context.blogs.OrderByDescending(b => b.Date).Take(9).ToList();
+            return View(blogs);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var blog = await _context.blogs.FindAsync(id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
         }
     }
 }
