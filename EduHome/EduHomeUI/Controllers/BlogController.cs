92c4340b7fa1fe5c544373b2c5f75a5ced023b56
Rahil Habibli <rahilah@code.edu.az>
2023-07-02 17:47:09 +0400

    Blogs Page View Done - Details Redirection Start

--- EduHome/EduHomeUI/Controllers/BlogController.cs
+++ EduHome/EduHomeUI/Controllers/BlogController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EduHomeUI.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly AppDbContext _context;
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
